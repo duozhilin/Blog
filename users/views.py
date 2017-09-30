44e4dc1e60fb832cd9af1a8e0b1d9ec9fa2b8bd3
eric <eric@yonglang.co>
2017-09-30 15:17:36 +0800

    完成注册功能

--- users/views.py
+++ users/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
-from django.contrib.auth import logout
-from django.contrib.auth.views import login
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.views import login as django_login_view
+from django.contrib.auth.models import UserManager
+from .models import User
 
 
 def login_view(request):
     """用户登录"""
     context = {}
     if request.method == 'POST':
-        login(request)
+        django_login_view(request)
         if not request.user.is_authenticated():
             context['error'] = 'error'
 
@@ -26,6 +28,23 @@ def register(request):
     """注册"""
     if request.method == 'POST':
         data = request.POST
-        print(data['username'])
+        user_manager = UserManager()
+        username_ = data['username']
+        email_ = data['email']
+        password_ = data['password']
+        nickname_ = data['nickname']
+
+        # new_user = user_manager.create_user(username_, email_, password_, nickname =nickname_)
+        new_user = User()
+        new_user.username = username_
+        new_user.nickname = nickname_
+        new_user.email = email_
+        new_user.set_password(password_)
+        new_user.save()
+
+        print(new_user)
+        authenticated_user = authenticate(username = username_, password = password_)
+        login(request, authenticated_user)
+        return HttpResponseRedirect(reverse('blogs:index'))
 
     return render(request, 'users/register.html')
